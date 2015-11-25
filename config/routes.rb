8222d3a548cfc74f80d1afe4258fce355fee2f35
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2015-11-26 10:50:17 +1100

    added faq page, not finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get "faq" => "faq#index"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
