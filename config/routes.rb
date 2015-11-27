0aaabfd011e2aaeb5a4e4082b921b96a0654064c
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2015-11-27 14:52:04 +1100

    updated curriculum overview and full curriculum

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get "faq" => "faq#index"
+  get "faq"         => "faq#index"
+  get "curriculum"  => "curriculum#index"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
