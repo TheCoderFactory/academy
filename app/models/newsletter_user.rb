39257cb64bb68372e99e6947e55d60d86cb30a90
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2015-12-15 11:22:57 +1100

    removed newsletter, only one font type

--- app/models/newsletter_user.rb
+++ app/models/newsletter_user.rb
@@ -1,5 +1,5 @@
 class NewsletterUser < ActiveRecord::Base
-  def subscribe_to_mailchimp
-    SubScribeToMailchimpJob.new.async.perform(self.id)
-  end
+  # def subscribe_to_mailchimp
+    # SubScribeToMailchimpJob.new.async.perform(self.id)
+  # end
 end
