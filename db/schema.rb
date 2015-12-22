5bd7e45fc20685aebd755b8b3e0a772fd4e2d912
pedorgrande <pete@thecoderfactory.com>
2015-12-23 04:59:08 +1100

    fixed info session modal

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,22 @@ ActiveRecord::Schema.define(version: 20151222044005) do
     t.string   "phone"
   end
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
+
   create_table "enquiries", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -65,8 +81,30 @@ ActiveRecord::Schema.define(version: 20151222044005) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "newsletter_users", force: :cascade do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.string   "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
+    t.string   "title"
+    t.text     "lead"
+    t.text     "content"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.boolean  "publish"
+    t.date     "published_date"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  create_table "press_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "lead"
     t.text     "content"
@@ -81,7 +119,10 @@ ActiveRecord::Schema.define(version: 20151222044005) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email",                  default: "", null: false
-    t.string   "image"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
