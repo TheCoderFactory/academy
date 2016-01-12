d94b0bcf5df483534d45b92e4e5158e332ee283c
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-01-12 15:12:59 +1100

    fixed blog posts error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,11 +21,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = Post.friendly.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = Post.friendly.find(params[:id])
     if @post.update_attributes(post_params)
       redirect_to @post
     else
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    @post = Post.friendly.find(params[:id])
     @post.destroy
     redirect_to "/posts"
   end
