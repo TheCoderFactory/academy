70dba57be23b9b58555334ad9d9b7798f0f4372c
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-01-14 13:51:38 +1100

    removed icon bar from side of page

--- config/routes.rb
+++ config/routes.rb
@@ -16,24 +16,25 @@ Rails.application.routes.draw do
 
   resources :women_in_tech_scholarships
   get "women-in-tech/apply"                   => "women_in_tech_scholarships#new"
-  get "women-in-tech/scholarship-information"  => "pages#women_in_tech_scholarship"
+  get "women-in-tech"  => "pages#women_in_tech_scholarship"
 
   resources :posts
+
   resources :info_session_attendees
   resources :info_session_dates
   get "information_sessions"   => "info_session_dates#index"
 
-  get "faq"                       => "pages#faq"
-  get "curriculum"                => "pages#curriculum"
-  get "about"                     => "pages#about"
-  get "about/instructors"         => "pages#instructors"
-  get "contact"                   => "pages#contact"
-  get "privacy"                   => "pages#privacy"
-  get "confirmation"              => "pages#confirmation"
-  get 'vet-fee-help'              => "pages#vet_fee_help"
-  get 'partners'                  => "pages#partners"
-  get 'testimonials'              => "pages#testimonials"
-  # get 'video-archive'             => "pages#video_archive"
+  get "frequently-asked-questions"    => "pages#faq"
+  get "curriculum"                    => "pages#curriculum"
+  get "about"                         => "pages#about"
+  get "about/instructors"             => "pages#instructors"
+  get "contact"                       => "pages#contact"
+  get "privacy"                       => "pages#privacy"
+  get "confirmation"                  => "pages#confirmation"
+  get 'vet-fee-help'                  => "pages#vet_fee_help"
+  get 'partners'                      => "pages#partners"
+  get 'testimonials'                  => "pages#testimonials"
+  # get 'video-archive'               => "pages#video_archive"
 
   get "blog"          => "blog#index"
   get "press"         => "press#index"
