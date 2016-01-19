f432d52135385d1cc4c6dd9211d073c27edd6ab2
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-01-19 11:36:08 +1100

    title updates and sitemap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @info_session_attendee = InfoSessionAttendee.new
-    @info_sessions = InfoSessionDate.all
-    @info_session_dates = Array.new
-    @info_sessions.each do |session|
-      @info_session_dates << session.session_date
-    end
-    @workshop_session_attendee = WorkshopAttendee.new
-    @workshop_sessions = WorkshopDate.all
-    @workshop_session_dates = Array.new
-    @workshop_sessions.each do |session|
-      @workshop_session_dates << session.session_date
-    end
   end
 end
