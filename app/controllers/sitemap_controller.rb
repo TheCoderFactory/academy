1517a72a6fcb72f910af91476066fec83ea4e317
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-01-19 15:30:05 +1100

    fixed sitemap invalid links

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -4,13 +4,13 @@ class SitemapController < ApplicationController
     @static_pages = [
       root_url,
       blog_url,
-      about_url,
+      about_coder_factory_academy_url,
       contact_url,
       privacy_url,
       academy_applications_url,
       women_in_tech_url,
       curriculum_url,
-      about_instructors_url,
+      about_coder_factory_academy_instructors_url,
       vet_fee_help_url,
       partners_url,
       testimonials_url
