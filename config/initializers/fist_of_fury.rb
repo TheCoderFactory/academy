b150a30cbc296856929a329673b28c3150ca6a26
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-02-16 11:27:49 +1100

    updated fist of fury initiallizer

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,7 +1,6 @@
 # Ensure the jobs run only in a web server.
 if defined?(Rails::Server)
   FistOfFury.attack! do
-    # Jobs can be scheduled here. Example:
-    # SayHiJob.recurs { secondly(3) }
+    UpdateSpreadsheetJob.recurs {daily.hour_of_day(9, 17)}
   end
 end
