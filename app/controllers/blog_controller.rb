5a5515027797cedda5b4ffa4cd8376a6e34da036
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-02-29 11:02:13 +1100

    blog side bar

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,13 @@
 class BlogController < ApplicationController
   def index
     @posts = Post.all.current_articles.paginate(:page => params[:page], :per_page => 5)
+    @info_sessions = InfoSessionDate.all
+    @info_session_dates = Array.new
+    @info_sessions.each do |session|
+      @info_session_dates << session.session_date.strftime('%d %B %Y') if session.session_date.future?
+    end
+    @info_session_dates.sort! { |a,b|  DateTime.parse(a) <=> DateTime.parse(b) }
+    @info_session_date = Array.new
+    @info_session_date << @info_session_dates.first
   end
 end
