2f4395f603ab06fe1245261566a2d1ca3c60f947
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-03-01 11:04:25 +1100

    Now actually shows next info session on blog post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,14 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.friendly.find(params[:id])
+    @info_sessions = InfoSessionDate.all
+    @info_session_dates = Array.new
+    @info_sessions.each do |session|
+      @info_session_dates << session.session_date.strftime('%d %B %Y') if session.session_date.future?
+    end
+    @info_session_dates.sort! { |a,b|  DateTime.parse(a) <=> DateTime.parse(b) }
+    @info_session_date = Array.new
+    @info_session_date << @info_session_dates.first
   end
 
   def update
