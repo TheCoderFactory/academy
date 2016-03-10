3513eed0b2bb080e7c4ee8c753026dda2ab5e6dc
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-03-10 13:53:20 +1100

    added script for course intakes

--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,5 @@ Rails.application.routes.draw do
   get "press"         => "press#index"
 
   get "update-spreadsheet"  => "update_spreadsheet#update"
+  get "move-course-intake"  => "update_spreadsheet#course_intake"
 end
