6aa07b38af034bd55e566c207c4407ab80955c90
Aaron Hook <ahoo2441@uni.sydney.edu.au>
2016-03-16 13:23:06 +1100

    fix to info sessions

--- app/controllers/info_session_attendees_controller.rb
+++ app/controllers/info_session_attendees_controller.rb
@@ -6,7 +6,7 @@ class InfoSessionAttendeesController < ApplicationController
     @info_sessions = InfoSessionDate.all
     @info_session_dates = Array.new
     @info_sessions.each do |session|
-      @info_session_dates << session.session_date.strftime('%d %B %Y') if session.session_date.future? || session.session_date.present?
+      @info_session_dates << session.session_date.strftime('%d %B %Y') if session.session_date.future? || session.session_date == Date.today
     end
     @info_session_dates.sort! { |a,b|  DateTime.parse(a) <=> DateTime.parse(b) }
     @info_session_date = Array.new
